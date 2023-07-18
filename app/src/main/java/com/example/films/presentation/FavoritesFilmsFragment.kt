9390f9ba1b51e946bbad7953bf052dd4decdfbaf
user <a.pykhonin@gmail.com>
2023-07-18 13:37:03 +0300

    Add navigation, update logo, fixed view binding in fragments

--- app/src/main/java/com/example/films/presentation/FavoritesFilmsFragment.kt
+++ app/src/main/java/com/example/films/presentation/FavoritesFilmsFragment.kt
@@ -1,10 +1,6 @@
 package com.example.films.presentation
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.films.R
 import com.example.films.databinding.FragmentFavoritesFilmsBinding
@@ -13,10 +9,5 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class FavoritesFilmsFragment : Fragment(R.layout.fragment_favorites_films) {
     private val binding by viewBinding(FragmentFavoritesFilmsBinding::bind)
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return binding.root
-    }
+
 }
--- app/src/main/java/com/example/films/presentation/FilmsListFragment.kt
+++ app/src/main/java/com/example/films/presentation/FilmsListFragment.kt
@@ -1,10 +1,6 @@
 package com.example.films.presentation
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.films.R
 import com.example.films.databinding.FragmentFilmsListBinding
@@ -14,10 +10,5 @@ import dagger.hilt.android.AndroidEntryPoint
 class FilmsListFragment : Fragment(R.layout.fragment_films_list) {
 
     private val binding by viewBinding(FragmentFilmsListBinding::bind)
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return binding.root
-    }
+
 }

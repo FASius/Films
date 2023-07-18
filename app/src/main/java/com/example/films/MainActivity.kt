2149ae8a10cca5a1b215c8ae25cdd787cbfd332b
user <a.pykhonin@gmail.com>
2023-07-18 13:00:22 +0300

    Initial

---
+++ app/src/main/java/com/example/films/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.films
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.films.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding: ActivityMainBinding by viewBinding(
+        ActivityMainBinding::bind,
+        R.id.activity_fragment_container
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
